50e034e96f8b7283678ad6e045a163ad87ecfd72
inksne <debanyj23@gmail.com>
2024-10-05 10:36:52 +0300

    weather-gismeteo

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 USE_ROUNDED_COORDS = True
 GISMETEO_URL = (
-    "https://api.gismeteo.net/v2/weather/current/?latitude={latitude}&longitude={longitude}&token=None"
+    "https://api.gismeteo.net/v2/weather/current/?latitude={latitude}&longitude={longitude}&token=a3748bb4-98ba-46f7-a95c-302d9f250869"
 )
